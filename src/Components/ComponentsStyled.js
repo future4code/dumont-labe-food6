4f5dec4ed4ed5e24c4d1d7c001e410fa8ed5459c
Rodrigo Xavier <rodrfx@gmail.com>
2020-12-07 18:23:55 -0300

    componente header

--- src/Components/ComponentsStyled.js
+++ src/Components/ComponentsStyled.js
@@ -10,11 +10,12 @@ align-items:center;
   export const Footer=styled.div`
   display:flex;
   justify-content:space-between;
-  background-color: #e86e5a;
   width:360px;
   height:49px;
   align-items:center;
   padding:5px;
+  box-sizing: border-box;
+  border: 1px solid black;
    `
 
  export const ImgLogo=styled.img`
@@ -28,3 +29,32 @@ align-items:center;
     width: 200px;
   height: 200px;
  `
+
+export const HeaderContainer=styled.div`
+    display: flex;
+    align-items: center;
+    width: 360px;
+    height: 64px
+    justify-content: space-between;
+    border: 1px solid black;
+    box-sizing: border-box;
+    `
+
+export const HeaderStart = styled.div`
+    display: flex;
+    justify-content: flex-start;
+    align-items: center;
+`
+
+export const HeaderMain = styled.div`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    padding-left: 50px;
+` 
+export const HeaderEnd = styled.div`
+    display: flex;
+    justify-content: flex-end;
+    align-items: center;
+    padding-left: 60px;
+`
