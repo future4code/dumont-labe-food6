664f3dafb1d35601a0e2896c8d4c2be45b19dec1
Vanessasz <vanessa182vans@gmail.com>
2020-12-08 16:57:13 -0300

    Fazendo a parte de feed os estilos

--- src/Screens/Home/FeedPage.js
+++ src/Screens/Home/FeedPage.js
@@ -2,12 +2,14 @@ import React from "react";
 import * as S from "../ScreenStyled"
 import Header from '../../Components/Header/Header'
 import Footer from '../../Components/Footer/Footer'
+import search from "../../Assets/search.svg"
 
 export default function FeedPage() {
     return (
       <S.Container>
       <Header title={"Rappi4"}/>
-      <S.RectangleFeed> </S.RectangleFeed>
+      <S.RectangleFeed><img img src = {search}/></S.RectangleFeed>
+      Burger 
       <Footer />
   </S.Container>
     )
